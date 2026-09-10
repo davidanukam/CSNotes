07ec2ffed5e370fab3e7af9c10e296aaa5f6935a
davidanukam <davidanukam72@gmail.com>
2026-09-10 17:10:05 -0400

    vault backup: 2026-09-10 17:10:05

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" data-scroll-behavior="smooth" suppressHydrationWarning>
       <body className={`${inter.variable} ${inter.className}`} suppressHydrationWarning>
         {children}
       </body>
